01334689103f13cd1ceeee12c846993191700436
bharathkreddy <reddybharath.k@gmail.com>
2023-11-26 16:30:34 +0000

    subscription id dupe removed

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,6 @@ provider "azurerm" {
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
 }
 
 data "azurerm_client_config" "current" {}
